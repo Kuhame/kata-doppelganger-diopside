f9579b425eaa6e3a4b93e1a7f52a7e3228aefcc2
Arthur <arrhur.buisson@gmail.com>
2025-01-08 16:52:51 +0100

    safe calculator text with mock

--- js/tests/safe-calculotor.test.js
+++ js/tests/safe-calculotor.test.js
@@ -1,10 +1,18 @@
 const SafeCalculator  = require('../safe-calculator');
 
 test('should not throw when authorized', () => {
+  // ####### PART 1 #######
 
-  const authorizer = {
-    authorize: () => true
-  }
-  const calculator = new SafeCalculator(authorizer)
-    calculator.add(1, 2)
+  // const authorizer = {
+  //   authorize: () => true
+  // }
+  // const calculator = new SafeCalculator(authorizer)
+  //   calculator.add(1, 2)
+
+
+  // ####### PART 2 #######
+
+  const mockAuthorizer = { authorize: jest.fn(() => true) };
+  const calculator = new SafeCalculator(mockAuthorizer);
+  expect(() => calculator.add(1, 2)).not.toThrow();
 })
